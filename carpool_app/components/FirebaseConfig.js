c43e86478174f5d265d71f6dd1124fe3aa9a4e58
Anton Roman Young <89807629+antonryoung02@users.noreply.github.com>
2024-02-22 08:14:05 -0600

    Edit .envs

--- carpool_app/components/FirebaseConfig.js
+++ carpool_app/components/FirebaseConfig.js
@@ -1,17 +1,18 @@
 // This file is how we connect to my Firebase Authentication project. 
 // I believe `firebaseConfig` is intended to be public.
+import { REACT_APP_FIREBASE_API_KEY, REACT_APP_AUTH_DOMAIN, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET, REACT_APP_MESSAGING_SENDER_ID, REACT_APP_APP_ID, REACT_APP_MEASUREMENT_ID} from '@env';
 
 import firebase, { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1wkqjWJtmPd46bWwyw28Oc0k-kVpfWcE",
-  authDomain: "ridealong-413818.firebaseapp.com",
-  projectId: "ridealong-413818",
-  storageBucket: "ridealong-413818.appspot.com",
-  messagingSenderId: "1046114662384",
-  appId: "1:1046114662384:web:b88a81b8fcf0595d890f35",
-  measurementId: "G-ENDFKDLT1B"
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID,
 };
 
 
