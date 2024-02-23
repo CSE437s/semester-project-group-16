596fd71b4429f65078987e17c86c310ee4e35c62
Hoonx <106128890+Hoonx@users.noreply.github.com>
2024-02-22 18:15:03 -0600

    Error Fixed

--- carpool_app/components/FirebaseConfig.js
+++ carpool_app/components/FirebaseConfig.js
@@ -1,9 +1,19 @@
-// This file is how we connect to my Firebase Authentication project. 
+// This file is how we connect to my Firebase Authentication project.
 // I believe `firebaseConfig` is intended to be public.
-import { REACT_APP_FIREBASE_API_KEY, REACT_APP_AUTH_DOMAIN, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET, REACT_APP_MESSAGING_SENDER_ID, REACT_APP_APP_ID, REACT_APP_MEASUREMENT_ID} from '@env';
+import {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID,
+  REACT_APP_MEASUREMENT_ID,
+} from '@env';
 
-import firebase, { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import firebase, { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+console.log(REACT_APP_FIREBASE_API_KEY);
 
 const firebaseConfig = {
   apiKey: REACT_APP_FIREBASE_API_KEY,
@@ -14,7 +24,6 @@ const firebaseConfig = {
   appId: REACT_APP_APP_ID,
   measurementId: REACT_APP_MEASUREMENT_ID,
 };
-
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 
