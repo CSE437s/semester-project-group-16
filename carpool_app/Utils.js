27c48f1803006c5fa34d7143fbd9a25afc96fb7f
Hoonx <106128890+Hoonx@users.noreply.github.com>
2024-02-27 00:28:07 -0600

    Calendar Function Added

--- carpool_app/Utils.js
+++ carpool_app/Utils.js
@@ -1,138 +1,141 @@
 //Contains API call functions
 import { FIREBASE_AUTH } from './components/FirebaseConfig';
-import { REACT_APP_LOCAL_SERVER, REACT_APP_REMOTE_SERVER} from '@env';
+import { REACT_APP_LOCAL_SERVER, REACT_APP_REMOTE_SERVER } from '@env';
 
 export const getUserRides = async () => {
-    try {
-      const user = checkUserExists();
-      const idToken = await user.getIdToken(true);
-      const apiUrl = `${REACT_APP_REMOTE_SERVER}/rides/${user.uid}`;
+  try {
+    const user = checkUserExists();
+    const idToken = await user.getIdToken(true);
+    const apiUrl = `${REACT_APP_REMOTE_SERVER}/rides/${user.uid}`;
 
-      const response = await fetch(apiUrl, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `${idToken}`,
-        },
-      });
-  
-      if (!response.ok) {
-        throw new Error('Failed to fetch from protected endpoint');
-      }
-      const responseData = await response.json();
-      console.log(`Got response data! ${JSON.stringify(responseData)}`)
-      return responseData;
-  
-    } catch (error) {
-      console.error('Error making API call:', error);
+    const response = await fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${idToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch from protected endpoint');
     }
-  };
-
+    const responseData = await response.json();
+    console.log(`Got response data! ${JSON.stringify(responseData)}`);
+    return responseData;
+  } catch (error) {
+    console.error('Error making API call:', error);
+  }
+};
 
 export const createNewUser = async () => {
-    console.log("Create new user called");
-    try {
-        const user = checkUserExists();
-        const idToken = await user.getIdToken(true);
-        //const apiUrl = `http://localhost:3000/users`;
-        const apiUrl = `${REACT_APP_REMOTE_SERVER}/users`;
-        const userId = user.uid;
-        const email = user.email;
-        const data = {userId:userId, email:email}
+  console.log('Create new user called');
+  try {
+    const user = checkUserExists();
+    const idToken = await user.getIdToken(true);
+    //const apiUrl = `http://localhost:3000/users`;
+    const apiUrl = `${REACT_APP_REMOTE_SERVER}/users`;
+    const userId = user.uid;
+    const email = user.email;
+    const data = { userId: userId, email: email };
 
-        const response = await fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `${idToken}`,
-        },
-        body: JSON.stringify(data),
-        });
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${idToken}`,
+      },
+      body: JSON.stringify(data),
+    });
 
-        if (!response.ok) {
-            throw new Error('Failed to fetch from protected endpoint');
-        }
-        const responseData = await response.json();
-        console.log(`Got response data! ${JSON.stringify(responseData)}`)
-        return responseData;
-
-    } catch (error) {
-        console.error('Error making API call:', error);
+    if (!response.ok) {
+      throw new Error('Failed to fetch from protected endpoint');
     }
+    const responseData = await response.json();
+    console.log(`Got response data! ${JSON.stringify(responseData)}`);
+    return responseData;
+  } catch (error) {
+    console.error('Error making API call:', error);
+  }
 };
 export const checkUserExists = () => {
-    const user = FIREBASE_AUTH.currentUser;
-    if (!user) {
-      throw new Error('User is not logged in');
-    }
-    return user;
-}
+  const user = FIREBASE_AUTH.currentUser;
+  if (!user) {
+    throw new Error('User is not logged in');
+  }
+  return user;
+};
 
-const createNewTrip = async (userId, originAddress, destinationAddress, category, completed, timestamp) => {
-    try {
-      const user = checkUserExists();
-      const idToken = await user.getIdToken(true);
-      const apiUrl = `${REACT_APP_REMOTE_SERVER}/trips`; 
+const createNewTrip = async (
+  userId,
+  originAddress,
+  destinationAddress,
+  category,
+  completed,
+  timestamp
+) => {
+  try {
+    const user = checkUserExists();
+    const idToken = await user.getIdToken(true);
+    const apiUrl = `${REACT_APP_REMOTE_SERVER}/trips`;
 
-      const response = await fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-           Authorization: `${idToken}`,
-          // Include any authentication headers if necessary
-        },
-        body: JSON.stringify({
-          userId: userId,
-          originAddress: originAddress,
-          destinationAddress: destinationAddress,
-          category: category,
-          completed: completed,
-          timestamp: timestamp,
-        }),
-      });
-  
-      const result = await response.json();
-  
-      if (response.ok) {
-        console.log('Trip created successfully:', result);
-        return result;
-      } else {
-        console.error('Failed to create trip:', result.message);
-        throw new Error(result.message);
-      }
-    } catch (error) {
-      console.error('Error creating new trip:', error);
-      throw error;
-    }
-  };
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${idToken}`,
+        // Include any authentication headers if necessary
+      },
+      body: JSON.stringify({
+        userId: userId,
+        originAddress: originAddress,
+        destinationAddress: destinationAddress,
+        category: category,
+        completed: completed,
+        timestamp: timestamp,
+      }),
+    });
 
-  const createNewStop = async (stopAddress, userId, routeId) => {
-    try {
-      const response = await fetch('http://<your-server-ip>:<port>/stops', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          // Include any authentication headers if necessary
-        },
-        body: JSON.stringify({
-          stopAddress: stopAddress,
-          userId: userId,
-          routeId: routeId, // Note: This can be null/undefined and should be handled by your backend accordingly
-        }),
-      });
-  
-      const result = await response.json();
-  
-      if (response.ok) {
-        console.log('Stop created successfully:', result);
-        return result;
-      } else {
-        console.error('Failed to create stop:', result.error);
-        throw new Error(result.error);
-      }
-    } catch (error) {
-      console.error('Error creating new stop:', error);
-      throw error;
+    const result = await response.json();
+
+    if (response.ok) {
+      console.log('Trip created successfully:', result);
+      return result;
+    } else {
+      console.error('Failed to create trip:', result.message);
+      throw new Error(result.message);
     }
-  };
-  
+  } catch (error) {
+    console.error('Error creating new trip:', error);
+    throw error;
+  }
+};
+
+const createNewStop = async (stopAddress, userId, routeId) => {
+  try {
+    const response = await fetch('http://<your-server-ip>:<port>/stops', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // Include any authentication headers if necessary
+      },
+      body: JSON.stringify({
+        stopAddress: stopAddress,
+        userId: userId,
+        routeId: routeId, // Note: This can be null/undefined and should be handled by your backend accordingly
+      }),
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      console.log('Stop created successfully:', result);
+      return result;
+    } else {
+      console.error('Failed to create stop:', result.error);
+      throw new Error(result.error);
+    }
+  } catch (error) {
+    console.error('Error creating new stop:', error);
+    throw error;
+  }
+};
--- carpool_app/screens/HomeScreen.js
+++ carpool_app/screens/HomeScreen.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator, TouchableOpacity} from 'react-native';
+import {
+  Modal,
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  TouchableOpacity,
+} from 'react-native';
+import { Calendar } from 'react-native-calendars';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import MapComponent from '../components/MapComponent';
 import 'leaflet/dist/leaflet.css';
-import {getUserRides} from '../Utils';
+import { getUserRides } from '../Utils';
 //import MapView, { Marker } from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 import { FIREBASE_AUTH } from '../components/FirebaseConfig';
@@ -11,14 +19,15 @@ import { FIREBASE_AUTH } from '../components/FirebaseConfig';
 const HomeScreen = () => {
   const [currentRegion, setCurrentRegion] = useState(null);
   //PLACEHOLDER POLYLINE!!
-  const [userRides, setUserRides] = useState([])
+  const [userRides, setUserRides] = useState([]);
   const [mapLoaded, setMapLoaded] = useState(false);
-
+  const [modalVisible, setModalVisible] = useState(false);
+  const [markedDates, setMarkedDates] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        getCurrentLocation(); 
+        getCurrentLocation();
         const rides = await getUserRides();
         if (rides) {
           setUserRides(rides);
@@ -28,7 +37,7 @@ const HomeScreen = () => {
         console.error(error);
       }
     };
-  
+
     fetchData();
   }, []);
 
@@ -44,25 +53,35 @@ const HomeScreen = () => {
             latitudeDelta: 0.0922, // Zoom level for latitude
             longitudeDelta: 0.0421, // Zoom level for longitude
           });
-
         },
         (error) => {
           console.log(error.code, error.message);
-          
         },
         { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
       );
     } else {
-      console.log("Geolocation is not supported by this browser.");
+      console.log('Geolocation is not supported by this browser.');
     }
   };
 
   const onDatePress = () => {
-    console.log("Date is pressed!");
-  }
+    console.log('Date is pressed!');
+  };
   const onManageCarpoolsPress = () => {
-    console.log("Manage carpools is pressed!");
-  }
+    console.log('Manage carpools is pressed!');
+    const newMarkedDates = userRides.reduce((acc, ride) => {
+      const dateKey = timestampToDate(ride.timestamp); // Convert each timestamp to date key
+      acc[dateKey] = {
+        selected: true,
+        marked: true,
+        selectedColor: 'blue',
+      };
+      return acc;
+    }, {});
+
+    setMarkedDates(newMarkedDates);
+    setModalVisible(true);
+  };
 
   return (
     <>
@@ -75,10 +94,36 @@ const HomeScreen = () => {
             </TouchableOpacity>
           </View>
           <MapComponent currentRegion={currentRegion} ride={userRides[0]} />
-          
+
           <TouchableOpacity onPress={onManageCarpoolsPress}>
             <Text>Manage Carpools</Text>
           </TouchableOpacity>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>Manage Carpools</Text>
+                <Calendar
+                  // ... add your other calendar properties
+                  markedDates={markedDates}
+                />
+                <TouchableOpacity
+                  style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
+                  onPress={() => {
+                    setModalVisible(!modalVisible);
+                  }}
+                >
+                  <Text style={styles.textStyle}>Hide Calendar</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
         </View>
       ) : (
         <ActivityIndicator />
@@ -88,33 +133,60 @@ const HomeScreen = () => {
 };
 
 const timestampToDate = (timestamp) => {
-  if (!timestamp) {
-    return ""
-  }
-  const date = new Date(timestamp * 1000);
-  const formattedDate = date.toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: true
-  });
-  return formattedDate;
-}
+  const date = new Date(timestamp);
+  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
+    2,
+    '0'
+  )}-${String(date.getDate()).padStart(2, '0')}`;
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems:'center',
+    alignItems: 'center',
     padding: 10,
-
   },
   tripInfo: {
-   flexDirection: 'row', 
-   justifyContent: 'space-around',
-   width: '60%'
-  }
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '60%',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  openButton: {
+    backgroundColor: '#F194FF',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
 });
 
 export default HomeScreen;
