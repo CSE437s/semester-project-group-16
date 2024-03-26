6454ded64074cad222d077b64de6ce1f26c0f6a6
Hoonx <106128890+Hoonx@users.noreply.github.com>
2024-03-25 21:29:23 -0500

    Schedule Page Done

--- carpool_app/components/Post.js
+++ carpool_app/components/Post.js
@@ -1,47 +1,75 @@
 import React from 'react';
 import styled from 'styled-components';
-import {checkUserExists, timestampToWrittenDate} from '../Utils';
+import { checkUserExists, timestampToWrittenDate } from '../Utils';
 import StopCreation from './StopCreation';
 import { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Modal, Picker, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  Modal,
+  Picker,
+  TouchableOpacity,
+} from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import Icon from 'react-native-vector-icons/Ionicons';
 import ShowPost from './ShowPost';
 
-const Post = ({ trip, fromManageCarpools=false }) => {
-
+const Post = ({ trip, fromManageCarpools = false }) => {
   const [showPost, setShowPost] = useState(false);
 
-
   const handleApply = () => {
-    setShowPost(true); 
+    setShowPost(true);
     const userObj = checkUserExists();
     const userId = userObj.uid;
-    
   };
 
   const handleClose = () => {
     setShowPost(false);
   };
+
+  const isTripPast = () => {
+    const tripDate = new Date(trip.timestamp);
+    const currentDate = new Date();
+    return tripDate < currentDate;
+  };
+
   return (
     <TouchableOpacity style={styles.container} onPress={handleApply}>
       <Text style={styles.email}>{trip.tripUserEmail}</Text>
-      <Text style={styles.timestamp}>{timestampToWrittenDate(trip.timestamp)}</Text>
+      <Text style={styles.timestamp}>
+        {timestampToWrittenDate(trip.timestamp)}
+      </Text>
 
-      <View style={{display:'flex', flexDirection:'row', gap:5}}>
-        <Icon name={"business-outline"} size={16}/>
+      <View style={{ display: 'flex', flexDirection: 'row', gap: 5 }}>
+        <Icon name={'business-outline'} size={16} />
         <Text style={styles.address}> {trip.route.originAddress}</Text>
       </View>
-      <View style={{display:'flex', flexDirection:'row', gap:5}}>
-        <Icon name={"flag-outline"} size={16}/>
+      <View style={{ display: 'flex', flexDirection: 'row', gap: 5 }}>
+        <Icon name={'flag-outline'} size={16} />
         <Text style={styles.address}> {trip.route.destinationAddress}</Text>
       </View>
-      <View style={{display:'flex', flexDirection:'row', justifyContent:'space-around', margin: 10}}>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          margin: 10,
+        }}
+      >
         <Text style={styles.category}>{trip.category}</Text>
-        <Text style={styles.completed}>{trip.completed ? 'Completed' : 'Upcoming'}</Text>
+        <Text style={styles.completed}>
+          {isTripPast() ? 'Past' : 'Upcoming'}
+        </Text>
       </View>
       <Modal visible={showPost} animationType="slide">
-        <ShowPost trip={trip} onClose={handleClose} fromManageCarpools={fromManageCarpools}/> 
+        <ShowPost
+          trip={trip}
+          onClose={handleClose}
+          fromManageCarpools={fromManageCarpools}
+        />
       </Modal>
     </TouchableOpacity>
   );
@@ -54,8 +82,8 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
     padding: 20,
     backgroundColor: '#FFFFFF',
-    borderColor:'black',
-    borderWidth:0.5,
+    borderColor: 'black',
+    borderWidth: 0.5,
   },
   tripInfo: {
     flex: 1,
@@ -73,14 +101,12 @@ const styles = StyleSheet.create({
   },
   address: {
     fontSize: 14,
-    width:'90%',
+    width: '90%',
   },
   timestamp: {
     fontFamily: 'Poppins-Black',
-  }
+  },
   // Add other styles here
 });
 
-
 export default Post;
-
